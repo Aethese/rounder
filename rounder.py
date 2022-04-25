d270f64b27a8a921ba5911a9a6f776f2eadcce1d
Aethese <59108437+Aethese@users.noreply.github.com>
2022-04-25 15:07:51 -0500

    its ugly but mainly works

--- rounder.py
+++ rounder.py
@@ -20,6 +20,8 @@ def round(number: float, round_place: int = 0):
 	first_number : int
 		if number is being rounded to a whole number than it will return rounded for whole number.
 		ex: 4.5 is returned as 5 or 4.4 is returned as 4
+	rounded_number : float
+		the actual rounded number that the user wants
 	'''
 	is_float = isinstance(number, float)
 	if not is_float:
@@ -28,13 +30,30 @@ def round(number: float, round_place: int = 0):
 	number_to_str = str(number)
 	split_number = number_to_str.split('.')
 	past_decimal = split_number[1]  # number(s) past the decimal
+	first_numbers = int(split_number[0])  # the whole number
 
 	if round_place == 0:
-		first_number = int(split_number[0])
 		if int(past_decimal) >= 5:
-			first_number += 1
-			return first_number
+			first_numbers += 1
+			return first_numbers
 		else:
-			return first_number
-
-	return split_number  # just for now :)
+			return first_numbers
+	else:  # round past decimal
+		if int(past_decimal[round_place]) >= 5:
+			zero_string = ''  # add a 1 to whatever place needs changed to be 1 higher
+			# the for range determines where the number that needs to be changed is
+			for i in range(round_place):  # TODO: use a diff method cuz this CAN be hella slow
+				if i + 1 == round_place:
+					zero_string += '1'
+				else:
+					zero_string += '0'
+			
+			zero_string = '0.' + zero_string
+			zero_string = float(zero_string)
+			old_past = float('0.' + past_decimal)
+			new_past_numbers = str(zero_string + old_past).split('.')[1]  # new numbers past decimal place
+			rounded_number = str(first_numbers) + '.' + str(new_past_numbers)[:round_place]
+			return float(rounded_number)
+		else:
+			rounded_number = str(first_numbers) + '.' + past_decimal[:round_place]
+			return float(rounded_number)
