273e6c1810e2c9b011766244a8b63a375d16554f
Aethese <59108437+Aethese@users.noreply.github.com>
2022-04-27 08:34:59 -0500

    comments and var name changes

--- tests.py
+++ tests.py
@@ -1,9 +1,9 @@
 import os
 import rounder
-os.system('cls' if os.name == 'nt' else 'clear')
+os.system('cls' if os.name == 'nt' else 'clear')  # don't ask
 
 
-try:
+try:  # Test 1
 	test_1a = rounder.round(4.5, 0)
 	test_1b = rounder.round(4.4)
 except Exception as e:
@@ -15,7 +15,7 @@ else:
 	print('Test 1 failed:', test_1a, test_1b)
 
 
-try:
+try:  # Test 2
 	test_2a = rounder.round(3.45631, 3)
 	test_2b = rounder.round(3.45691, 3)
 except Exception as e:
@@ -27,7 +27,7 @@ else:
 	print('Test 2 failed:', test_2a, test_2b)
 
 
-try:
+try:  # Test 3
 	test_3a = rounder.round(3.45112, 1)
 	test_3b = rounder.round(3.41112, 1)
 except Exception as e:
@@ -39,7 +39,7 @@ else:
 	print('Test 3 failed:', test_3a, test_3b)
 
 
-try:
+try:  # Test 4
 	test_4a = rounder.round('penis', 1)
 	test_4b = rounder.round('yes')
 except Exception as e:
@@ -51,6 +51,6 @@ else:
 	print('Test 4 failed:', test_4a, test_4b)
 
 
-custom_test_a = float(input('Custom test: '))
-custom_test_b = int(input('What digit place? '))
-print(rounder.round(custom_test_a, custom_test_b))
+custom_test_5a = float(input('Custom test: '))  # Test 5
+custom_test_5b = int(input('What digit place? '))
+print(rounder.round(custom_test_5a, custom_test_5b))
